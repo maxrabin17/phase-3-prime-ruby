ba6d15300d70f6f599a03a783baee29519436763
maxrabin <max.rabin17@gmail.com>
2021-06-23 17:25:18 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
 # Add  code here!
+def prime?(integer)
+    if integer < 0 or integer == 0 or integer == 1
+        return false
+    else
+        (2..integer - 1).to_a.all? do |pot_prime|
+            integer % pot_prime != 0
+        end
+    end
+end
